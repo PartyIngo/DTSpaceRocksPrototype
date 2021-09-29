30168276b3c9d6a60fa51804e06e977251fcb50b
Marco Stano <m.stano@gmx.de>
2021-09-29 18:15:50 +0200

    Created Asteroid
    First iteration of the Asteroid Enemy.

---
+++ Assets/Enemies/Asteroid/AsteroidBehaviour.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidBehaviour : MonoBehaviour
+{
+    #region Variables
+    [Header("Asteroid Variables")]
+
+    [Tooltip("Maximum movement force of the Asteroid.")]
+    public float forceMax;
+    [Tooltip("Actual force of the Asteroid.")]
+    Vector2 force;
+
+    [Tooltip("Health of the Asteroid. If it reaches 0, the Asteroid is destroyed.")]
+    public int health;
+
+    Rigidbody2D rb;
+
+    [Tooltip("Maximum X-Coord.")]
+    public float Xmax;
+    [Tooltip("Maximum Y-Coord.")]
+    public float Ymax;
+    #endregion
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        force = new Vector2(Random.Range(-forceMax, forceMax), Random.Range(-forceMax, forceMax));
+        rb = GetComponent<Rigidbody2D>();
+        rb.AddForce(force);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 tmp = transform.position;
+
+        if (transform.position.x > Xmax)
+        {
+            tmp.x = -Xmax + 1;   
+            transform.position = tmp;
+        }
+
+        if (transform.position.x < -Xmax)
+        {
+            tmp.x = Xmax - 1; 
+            transform.position = tmp;
+        }
+
+        if (transform.position.y > Ymax)
+        {
+            tmp.y = -Ymax + 1;
+            transform.position = tmp;
+        }
+
+        if (transform.position.y < -Ymax)
+        {
+            tmp.y = Ymax - 1;
+            transform.position = tmp;
+        }
+
+    }
+}
