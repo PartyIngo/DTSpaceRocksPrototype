6cfe454c9414a51cdc06bd889d64f0f8592fea83
PartyIngo <meyerlukas96@gmx.de>
2021-10-11 16:07:26 +0200

    Asteroid VFX and Sprites
    implemennted basic burst VFX and sprite assignment

--- Assets/Enemies/Asteroid/AsteroidBehaviour.cs
+++ Assets/Enemies/Asteroid/AsteroidBehaviour.cs
@@ -22,6 +22,22 @@ public class AsteroidBehaviour : MonoBehaviour
     [Tooltip("Maximum Y-Coord.")]
     public float Ymax;
 
+
+    [Header("Asteroid Appearance")]
+
+    [Tooltip("The Size of the Asteroid. The larger the value, the larger the size.")]
+    [Range(1, 3)]
+    public int asteroidSize;
+    [Tooltip("The Sprite Renderer Component")]
+    SpriteRenderer spriteRenderer;
+    [Tooltip("Every Asteroid Sprite that may be assigned")]
+    public Sprite[] asteroidSprite;
+    [Tooltip("The assigned sprite.")]
+    [HideInInspector]
+    public int spriteIndex;
+    [Tooltip("The VFX that plays, when the asateroid gets destroyed")]
+    public GameObject burstVFX;
+
     #endregion
 
     // Start is called before the first frame update
@@ -30,6 +46,13 @@ public class AsteroidBehaviour : MonoBehaviour
         force = new Vector2(Random.Range(-forceMax, forceMax), Random.Range(-forceMax, forceMax));
         rb = GetComponent<Rigidbody2D>();
         rb.AddForce(force);
+
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+        //Choose and assign the sprite for the asteroid
+        changeAppearance();
+        
+
     }
 
     // Update is called once per frame
@@ -62,7 +85,27 @@ public class AsteroidBehaviour : MonoBehaviour
         }
     }
 
-    //Damage & Destroy VFX
+
+
+    /**
+     * Channge appearance of Asteroid, when it's the largest version of it
+     */
+    void changeAppearance()
+    {
+        if (asteroidSize == 3)
+        {
+            //assign random sprite
+            int rand = Random.Range(0, asteroidSprite.Length);
+            spriteRenderer.sprite = asteroidSprite[rand];
+            spriteIndex = rand;
+        }
+    }
+
+
+
+    /**
+     * Damage & Destroy VFX
+     */
     public void Damage(float damage)
     {
         print("Asteroid Damage:  " + damage);
@@ -73,6 +116,13 @@ public class AsteroidBehaviour : MonoBehaviour
         {
             GameObject handler = GameObject.Find("SpawnHandler");
             handler.gameObject.SendMessage("decreaseOverallWeight", "Asteroid Large");
+
+
+            //TODO: VFX explosion
+            Instantiate(burstVFX, transform.position, transform.rotation);
+
+
+            //TODO: Spawn child asteroids
 
 
             Destroy(gameObject);
